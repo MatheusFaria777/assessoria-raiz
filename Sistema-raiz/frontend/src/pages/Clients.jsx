7a09dd510cd1bedcc393e6ab08dd21386f44ffd3
Matheus Faria <mkt.matheusfaria@gmail.com>
2026-07-13 14:20:28 -0300

    auto-sync: 2026-07-13 14:20

--- Sistema-raiz/frontend/src/pages/Clients.jsx
+++ Sistema-raiz/frontend/src/pages/Clients.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { api } from '../lib/api'
 import { toast } from '../lib/toast'
 import ClientModal from '../components/clients/ClientModal'
